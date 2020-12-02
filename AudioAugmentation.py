35ec007c4e86a669ee8573312f96152bfc28775f
Ahmed Fakhry <44531149+AhmedFakhry47@users.noreply.github.com>
2020-12-02 17:02:38 +0200

    Added Random Augmentation Method

--- AudioAugmentation.py
+++ AudioAugmentation.py
@@ -3,31 +3,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class AudioAugmentation:
-  def shuffle_name(self,number=8,name=''):
-    names = []
-    for i in range(number):
-      names.append(''.join(random.sample(name,len(name))))
-    return names  
+  def shuffle_name(self,name=''):
+    return ''.join(random.sample(name,len(name)))
 
-  def read_audio_file(self, file_path):
-      data     = librosa.core.load(file_path)[0]
-      self.name= file_path.split('/')[-1].split('.')[0]
-      return data
+  def __init__(self, file_path,number_of_shuffles=1):
+    self.data = librosa.core.load(file_path)[0]
+    self.name = file_path.split('/')[-1].split('.')[0]
+    self.dir  = '/'.join(file_path.split('/')[:-1])
 
   def write_audio_file(self, file, data, sample_rate=44000):
-      data.export(file, bitrate = sample_rate,format = "mp3")
+    data.export(file, bitrate = sample_rate,format = "mp3")
 
-  def add_noise(self, data):
-      noise = np.random.randn(len(data))
-      data_noise = data + 0.005 * noise
-      return data_noise
+  def add_noise(self, data,parameter = 0.005):
+    noise = np.random.randn(len(data))
+    data_noise = data + parameter * noise
+    return data_noise
 
-  def shift(self, data):
-      return np.roll(data, 1600)
+  def shift(self, data,roller=1600):
+    return np.roll(data, roller)
 
   def stretch(self, data, rate=1):
-      data = librosa.effects.time_stretch(data, rate)
-      return data
+    data = librosa.effects.time_stretch(data, rate)
+    return data
+
+  def random_augmentation(self):
+    shoot = random.randint(0,3)
+    if (shoot ==1):
+      self.augdata = self.add_noise(self.data)
+    elif (shoot ==2):
+      self.augdata = self.shift(self.data)
+    elif (shoot ==3):
+      self.augdata = self.stretch(self.data)
+    
+    new_name = self.shuffle_name(self.name)
+    new_dir  = os.path.join(self.dir,new_name)
+    self.write_audio_file(new_dir,self.augdata)
+
+
 
 # Create a new instance from AudioAugmentation class
 aa = AudioAugmentation()
